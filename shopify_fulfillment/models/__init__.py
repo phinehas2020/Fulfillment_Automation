83bd262c33b6687533afe83d16eca13376895010
Phinehas Adams <phinehasmadams@icloud.com>
2025-12-05 13:49:01 -0600

    Reorder model imports to ensure base models load first

--- shopify_fulfillment/models/__init__.py
+++ shopify_fulfillment/models/__init__.py
@@ -1,8 +1,8 @@
-from . import shopify_order  # noqa: F401
-from . import shopify_order_line  # noqa: F401
 from . import fulfillment_box  # noqa: F401
 from . import fulfillment_shipment  # noqa: F401
 from . import print_job  # noqa: F401
 from . import print_test_wizard  # noqa: F401
+from . import shopify_order  # noqa: F401
+from . import shopify_order_line  # noqa: F401
 
 
