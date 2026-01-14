6a235f3f6ef93a1b04e4abc1147fbbb11a74ef3a
Phinehas Adams <phinehasmadams@icloud.com>
2026-01-14 14:41:31 -0600

    Fix model loading order: import shopify_config first

--- shopify_fulfillment/models/__init__.py
+++ shopify_fulfillment/models/__init__.py
@@ -1,10 +1,8 @@
+from . import shopify_config  # noqa: F401 - must be first for security CSV
 from . import fulfillment_box  # noqa: F401
 from . import fulfillment_shipment  # noqa: F401
 from . import print_job  # noqa: F401
 from . import print_test_wizard  # noqa: F401
 from . import shopify_order  # noqa: F401
 from . import shopify_order_line  # noqa: F401
-from . import shopify_config  # noqa: F401
 from . import res_config_settings  # noqa: F401
-
-
